23384588d0fda53f687f83cb13915c8fff5496fa
Sobhit Kumar <sobhit2010@gmail.com>
2018-10-13 22:53:24 +0530

    setup boards component

--- src/app/boards/boards.component.ts
+++ src/app/boards/boards.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DataSource } from '@angular/cdk/collections';
+import { FirebaseFirestoreService } from '../services/firebase-firestore.service';
 
 @Component({
   selector: 'app-boards',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./boards.component.css']
 })
 export class BoardsComponent implements OnInit {
+  displayedColumns = ['title', 'description', 'author'];
+  dataSource = new BoardDataSource(this.firebaseFirestoreService);
 
-  constructor() { }
+  constructor(private firebaseFirestoreService: FirebaseFirestoreService) { }
 
   ngOnInit() {
   }
 
+
+
+}
+
+export class BoardDataSource extends DataSource<any> {
+
+  constructor(private firebaseFirestoreService: FirebaseFirestoreService) {
+    super();
+  }
+
+  connect() {
+    return this.firebaseFirestoreService.getBoards();
+  }
+
+  disconnect() {
+
+  }
 }
