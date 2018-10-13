09a568d6fe763767a3fc5a94cb22ef558ee6b8f5
Sobhit Kumar <sobhit2010@gmail.com>
2018-10-13 22:25:18 +0530

    add CRUD functionality for firestore in firebase service

---
+++ src/app/services/firebase-firestore.service.ts
@@ -0,0 +1,74 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import * as firebase from 'firebase';
+import firestore from 'firebase/firestore';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FirebaseFirestoreService {
+  // Declare a variable for accessing Firestore collection before the constructor.
+  ref = firebase.firestore().collection('boards');
+
+  constructor() { }
+
+  getBoards(): Observable<any> {
+    return new Observable((observer) => {
+      this.ref.onSnapshot((querySnapshot) => {
+        const boards = [];
+        querySnapshot.forEach((doc) => {
+          const data = doc.data();
+          boards.push({
+            key: doc.id,
+            title: data.title,
+            description: data.description,
+            author: data.author
+          });
+        });
+        observer.next(boards);
+      });
+    });
+  }
+
+  getBoard(id: string): Observable<any> {
+    return new Observable((observer) => {
+      this.ref.doc(id).get().then((doc) => {
+        const data = doc.data();
+        observer.next({
+          key: doc.id,
+          title: data.title,
+          description: data.description,
+          author: data.author
+        });
+      });
+    });
+  }
+
+  postBoards(data): Observable<any> {
+    return new Observable((observer) => {
+      this.ref.add(data).then((doc) => {
+        observer.next({
+          key: doc.id,
+        });
+      });
+    });
+  }
+
+  updateBoards(id: string, data): Observable<any> {
+    return new Observable((observer) => {
+      this.ref.doc(id).set(data).then(() => {
+        observer.next();
+      });
+    });
+  }
+
+  deleteBoards(id: string): Observable<{}> {
+    return new Observable((observer) => {
+      this.ref.doc(id).delete().then(() => {
+        observer.next();
+      });
+    });
+  }
+}
