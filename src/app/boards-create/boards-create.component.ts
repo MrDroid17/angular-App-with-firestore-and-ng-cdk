95b9726ddef709339ed90999e39e8adac8fc57e2
Sobhit Kumar <sobhit2010@gmail.com>
2018-10-14 01:12:13 +0530

    add create and delete functionality

--- src/app/boards-create/boards-create.component.ts
+++ src/app/boards-create/boards-create.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FirebaseFirestoreService } from '../services/firebase-firestore.service';
+import { FormControl, FormGroupDirective, FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-boards-create',
@@ -6,10 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./boards-create.component.css']
 })
 export class BoardsCreateComponent implements OnInit {
+  boardsForm: FormGroup;
+  title: '';
+  description: '';
+  author: '';
 
-  constructor() { }
+  constructor(
+    private firebaseFirestoreService: FirebaseFirestoreService,
+    private router: Router,
+    private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.boardsForm = this.formBuilder.group({
+      'title': [null, Validators.required],
+      'description': [null, Validators.required],
+      'author': [null, Validators.required]
+    });
+  }
+
+
+  onFormSubmit(form: NgForm) {
+    this.firebaseFirestoreService.postBoards(form)
+      .subscribe(res => {
+        const id = res['key'];
+        this.router.navigate(['/boards-details', id]);
+      }, (err) => {
+        console.log(err);
+      });
   }
 
 }
--- src/app/boards-edit/boards-edit.component.ts
+++ src/app/boards-edit/boards-edit.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FirebaseFirestoreService } from '../services/firebase-firestore.service';
 
 @Component({
   selector: 'app-boards-edit',
@@ -6,10 +8,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./boards-edit.component.css']
 })
 export class BoardsEditComponent implements OnInit {
+  board = {};
 
-  constructor() { }
+  constructor(
+    private firebaseFirestoreService: FirebaseFirestoreService,
+    private route: ActivatedRoute,
+    private router: Router) { }
+
 
   ngOnInit() {
+    this.getBoardDetails(this.route.snapshot.params['id']);
   }
 
+  // get board details
+  getBoardDetails(id) {
+    this.firebaseFirestoreService.getBoard(id)
+      .subscribe(data => {
+        console.log(data);
+        this.board = data;
+      });
+  }
+
+  // deleate board details
+  deleteBoard(id) {
+    this.firebaseFirestoreService.deleteBoards(id)
+      .subscribe(res => {
+          this.router.navigate(['/boards']);
+        }, (err) => {
+          console.log(err);
+        }
+      );
+  }
 }
